2a2ee358c886b718676504d282df60eef64ea1bb
porkbrain <git@porkbrain.com>
2022-12-19 23:38:36 +0100

    Publish script automatically gets a gas object from CLI if there's no GAS env defined

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,8 +4,22 @@
 # Publishes a package into a new address. Assumes that Sui is running.
 #
 
-export $(cat .env | xargs)
+env=$(cat .env)
+if [ -n "${env}" ]; then
+    export $(echo "${env}" | xargs)
+fi
 
-sui client publish \
-    --gas "${GAS}" \
-    --gas-budget 30000 .
+# if GAS is not defined and jq dependency is defined, grab first gas object
+if [ -z "${GAS}" ] && [ -x "$(command -v jq)" ]; then
+    GAS="$(sui client gas --json | jq -r '.[0].id.id')"
+fi
+
+read -p "Use ${GAS} as gas object? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sui client publish \
+        --gas "${GAS}" \
+        --gas-budget 30000 .
+else
+    echo "Aborting"
+fi
