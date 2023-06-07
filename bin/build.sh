6742fa66772524a7c68ae152d34e2ce0093dc59f
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-06-07 18:20:31 +0100

    Fix CI

--- bin/build.sh
+++ bin/build.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
+if [ "$1" = "remote" ]; then
+    echo "Running script in remote setup"
+    sui="./../sui"
+else
+    echo "Running script in local setup"
+    sui="sui"
+fi
+
+
 cd ./contracts/allowlist
-sui move build
+${sui} move build
 cd ..
 
 cd ./authlist
-sui move build
+${sui} move build
 cd ..
 
 cd ./permissions
-sui move build
+${sui} move build
 cd ..
 
 cd ./pseudorandom
-sui move build
+${sui} move build
 cd ..
 
 cd ./utils
-sui move build
+${sui} move build
 cd ..
 
 cd ./kiosk
-sui move build
+${sui} move build
 cd ..
 
 cd ./launchpad
-sui move build
+${sui} move build
 cd ..
 
 cd ./liquidity_layer_v1
-sui move build
+${sui} move build
 cd ..
 
 cd ./liquidity_layer
-sui move build
+${sui} move build
 cd ..
 
 cd ./nft_protocol
-sui move build
+${sui} move build
 
 cd ../..
